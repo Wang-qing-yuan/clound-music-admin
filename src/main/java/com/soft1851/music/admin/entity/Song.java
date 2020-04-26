fa7a5610a7c82817cccdaf3732e4b76f11d0ce3d
wqy <1950016253@qq.com>
2020-04-26 16:48:54 +0800

    音乐管理

--- src/main/java/com/soft1851/music/admin/entity/Song.java
+++ src/main/java/com/soft1851/music/admin/entity/Song.java
@@ -4,8 +4,7 @@ import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.baomidou.mybatisplus.extension.activerecord.Model;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
+import lombok.*;
 import lombok.experimental.Accessors;
 
 import java.io.Serializable;
@@ -20,6 +19,9 @@ import java.time.LocalDateTime;
  * @since 2020-04-21
  */
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @EqualsAndHashCode(callSuper = false)
 @Accessors(chain = true)
 @TableName("song")
